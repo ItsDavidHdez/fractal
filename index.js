57fee83c6eeefcca1721856d4f5f351af5abf33f
David Vargas Hernandez <david-0610@live.com.mx>
2020-10-28 16:02:50 -0600

    V1

---
+++ index.js
@@ -0,0 +1,47 @@
+const draw = document.getElementById("canvas");
+const dimention = draw.getContext("2d");
+draw.width = window.innerWidth;
+canvas.height = window.innerHeight;
+
+const maxLevel = 5;
+const branches = 2;
+const sides = Math.floor(Math.random() * 10 + 3);
+const spread = Math.random() * 48 + 0.51;
+
+dimention.translate(draw.width / 2, draw.height / 2);
+
+const angle = Math.PI * 2 * spread;
+
+const drawLine = (level) => {
+  if (level > maxLevel) return;
+
+  dimention.strokeStyle = "#FFF";
+  dimention.lineWidth = 2;
+  dimention.beginPath();
+  dimention.moveTo(0, 0);
+  dimention.lineTo(50, 0);
+  dimention.stroke();
+
+  for (let i = 1; i < branches + 1; i++) {
+    dimention.save();
+    dimention.translate((150 * i) / (branches + 1), 0);
+    dimention.scale(0.5, 0.5);
+    dimention.save();
+
+    dimention.rotate(angle);
+    drawLine(level + 1);
+    dimention.restore();
+    dimention.save();
+
+    dimention.rotate(-angle);
+    drawLine(level + 1);
+    dimention.restore();
+
+    dimention.restore();
+  }
+};
+
+for (let i = 0; i < 5; i++) {
+  drawLine(0);
+  dimention.rotate((Math.PI * 2) / 5);
+}
